634e90c2484d006492fa6a30a7b86bd9db1362dc
Muhannad Ashraf <mu7nnd.as@gmail.com>
2023-12-24 19:22:59 +0200

    1758 solution, python

--- leetcode/1758.MinimumChangesToMakeAlternatingBinaryString/Minimum ChangesToMakeAlternatingBinaryString.py
+++ leetcode/1758.MinimumChangesToMakeAlternatingBinaryString/Minimum ChangesToMakeAlternatingBinaryString.py
@@ -1,4 +1,22 @@
 def minOperations(s):
+    # another solution
+    """def count(s, c):
+        count = 0
+
+        for i in range(1, len(s)):
+            current = s[i]
+            if c == current:
+                count += 1
+                c = '0' if c == '1' else '1'
+            else:
+                c = current
+        return count
+
+    c = s[0]
+    count1 = count(s, c)
+    count2 = count(s, '0' if c == '1' else '1') + 1
+
+    return min(count1, count2)"""
 
     count = 0
 
@@ -11,28 +29,7 @@ def minOperations(s):
     return min(count, len(s) - count)
 
 
-    # another solution
-    """def count(s, c):
-        count = 0
-        
-        for i in range(1, len(s)):
-            current = s[i]
-            if c == current:
-                count += 1
-                c = '0' if c == '1' else '1'
-            else:
-                c = current
-        return count
-    
-    c = s[0]
-    count1 = count(s, c)
-    count2 = count(s, '0' if c == '1' else '1') + 1
-    
-    return min(count1, count2)"""
-    
-
 if __name__ == '__main__':
     s = "1111"
     print(minOperations(s))
     # 2
-    
