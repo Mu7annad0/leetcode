bf9b5fd25bfcf759f0cf88f559dc9702a8d1250e
Muhannad Ashraf <mu7nnd.as@gmail.com>
2023-12-27 22:48:02 +0200

    1155 solution, python

--- leetcode/1155.NumberOfDiceRollsWithTargetSum/NumberOfDiceRollsWithTargetSum.py
+++ leetcode/1155.NumberOfDiceRollsWithTargetSum/NumberOfDiceRollsWithTargetSum.py
@@ -1,18 +1,28 @@
 def numRollsToTarget(n, k, target):
 
+    MOD = 10 ** 9 + 7
+    cashe = {}
 
-    dp = [[0] * (target+1) for _ in range(n+1)]
-    dp[0][0] = 1
+    def count(n, target):
 
-    for dice in range(1, n+1):
-        for tgt in range(target+1):
-            dp[dice][tgt] = sum(dp[dice-1][tgt-roll] for roll in range(1, min(tgt, k)+1))
+        if n == 0:
+            return 1 if target == 0 else 0
 
-    return dp[n][target]
+        if (n, target) in cashe:
+            return cashe[(n, target)]
+
+        res = 0
+        for val in range(1, k + 1):
+            res = (res + count(n - 1, target - val)) % MOD
+
+        cashe[(n, target)] = res
+        return res
+
+    return count(n, target)
 
 if __name__ == "__main__":
     target = 7
     n = 2
     k = 6
-    #dp = [[0] * (target + 1) for _ in range(n + 1)]
     print(numRollsToTarget(n, k, target))
+    # 6
