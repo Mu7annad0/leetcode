be85d8f99557722546b28ed13a8a054675dd0e7b
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-04-29 22:11:09 +0300

    0389 solution, python

--- leetcode/0389.FindTheDifference/FindTheDifference.py
+++ leetcode/0389.FindTheDifference/FindTheDifference.py
@@ -1,19 +1,7 @@
 def find_the_difference(s, t):
-
-    """
-    The function takes two strings, s and t, as input and returns a single character that appears in either string but not both.
-    It does this by iterating through each character in the longer string (either s or t) and checking its count in the other string.
-    If a character is found to have a different count in the two strings, it is returned immediately.
-    """
-
-    if len(s) > len(t):
-        for char in s:
-            if s.count(char) > t.count(char):
-                return char
-    if len(s) < len(t):
-        for char in t:
-            if s.count(char) < t.count(char):
-                return char
+    for char in t:
+        if s.count(char) < t.count(char):
+            return char
 
 
 if __name__ == "__main__":
