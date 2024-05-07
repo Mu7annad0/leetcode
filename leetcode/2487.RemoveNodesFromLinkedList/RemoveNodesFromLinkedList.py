ef0a4422752e661b3daccbea48c838306e276500
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-05-07 10:49:24 +0300

    2487 solution, python

--- leetcode/2487.RemoveNodesFromLinkedList/RemoveNodesFromLinkedList.py
+++ leetcode/2487.RemoveNodesFromLinkedList/RemoveNodesFromLinkedList.py
@@ -6,10 +6,13 @@
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
@@ -36,11 +39,22 @@ class Solution:
         return reverse(head)
 
 
-# Example 1:
+# Create the ListNode for [1, 8, 9]
+head = ListNode(5)
+head.next = ListNode(2)
+head.next.next = ListNode(13)
+head.next.next.next = ListNode(3)
+head.next.next.next.next = ListNode(8)
 
-# Input: head = [5,2,13,3,8]
-# Output: [13,8]
-# Explanation: The nodes that should be removed are 5, 2 and 3.
-# - Node 13 is to the right of node 5.
-# - Node 13 is to the right of node 2.
-# - Node 8 is to the right of node 3.
+# Execute the function with the created ListNode
+solution = Solution()
+result = solution.removeNodes(head)
+
+# Print the result
+current = result
+while current is not None:
+    print(current.val)
+    current = current.next
+
+# 13
+# 8
