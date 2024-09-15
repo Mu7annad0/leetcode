d41e36aed1eabdade75102d36ebddda10fcc25e2
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-09-16 00:00:30 +0300

    0122 solution, python

--- leetcode/0122.BestTImeToBuyAndSellStockII/BestTImeToBuyAndSellStockII.py
+++ leetcode/0122.BestTImeToBuyAndSellStockII/BestTImeToBuyAndSellStockII.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
@@ -12,14 +13,12 @@ class Solution:
         """
         profit = 0
         for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
         return profit
-    
+
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.maxProfit([7,1,5,3,6,4]))
+    print(s.maxProfit([7, 1, 5, 3, 6, 4]))
     # 7
-            
-                
