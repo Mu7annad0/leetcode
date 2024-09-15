2522ca83c46f973ca842d55917e859da21690372
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-09-15 23:52:55 +0300

    0122 solution, python

---
+++ leetcode/0122.BestTImeToBuyAndSellStockII/BestTImeToBuyAndSellStockII.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+        On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+        However, you can buy it then immediately sell it on the same day.
+
+        Find and return the maximum profit you can achieve.
+        """
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxProfit([7,1,5,3,6,4]))
+    # 7
+            
+                
