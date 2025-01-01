1e8bf8463237b426cb2e54a24ee3163469c570b9
Muhannad Ashraf <mu7nnd.as@gmail.com>
2025-01-01 23:21:04 +0200

    1422 solution, python

--- leetcode/1422.MaximumScoreAfterSplittingAString/MaximumScoreAfterSplittingA String.py
+++ leetcode/1422.MaximumScoreAfterSplittingAString/MaximumScoreAfterSplittingA String.py
@@ -1,10 +1,14 @@
-def maxScore(s):
-    score = 0
-    for i in range(1, len(s)):
-        score = max(score, s[:i].count('0') + s[i:].count('1'))
-    return score
-
+class Solution:
+    def maxScore(self, s: str) -> int:
+        score = 0
+        for i in range(1, len(s)):
+            score = max(score, s[:i].count('0') + s[i:].count('1'))
+        return score
+    
 
 if __name__ == '__main__':
+    # Test the function
     s = "011101"
-    print(maxScore(s))
+    sol = Solution()
+    print(sol.maxScore(s))
+    # Expected output: 5
